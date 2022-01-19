538123ccc17c35ae5154ed8104cebb6c1fa4ffb0
ygg <123@ygg.com>
2022-01-19 16:19:22 +0800

    使用generics

--- snippets/urls.py
+++ snippets/urls.py
@@ -20,13 +20,25 @@ from snippets.views import original_views
 # urlpatterns = format_suffix_patterns(urlpatterns)
 
 
-# 使用minxin
+# # 使用minxin
+# from django.urls import path
+# from rest_framework.urlpatterns import format_suffix_patterns
+# from snippets.views import mixin_views
+#
+# urlpatterns = [
+#     path('snippets/', mixin_views.SnippetList.as_view()),
+#     path('snippets/<int:pk>/', mixin_views.SnippetDetail.as_view()),
+# ]
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+
+# 使用 generics
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from snippets.views import mixin_views
+from snippets.views import generics_views
 
 urlpatterns = [
-    path('snippets/', mixin_views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', mixin_views.SnippetDetail.as_view()),
+    path('snippets/', generics_views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', generics_views.SnippetDetail.as_view()),
 ]
 urlpatterns = format_suffix_patterns(urlpatterns)
